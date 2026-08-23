d4d5a4d41ed58b13121d24f822952e63315ca362
jhanmesa-wq <jhanmesa@inkascollege.edu.pe>
2026-08-23 07:33:51 -0500

    Update bot username and name in main.py

--- main.py
+++ main.py
@@ -800,6 +800,7 @@ def main():
     logger.info(f"Flask {PORT}")
     app=Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start",start))
+        app.add_handler(CommandHandler("micelular",micelular))
     app.add_handler(CommandHandler("cmds",cmds_command))
     app.add_handler(CommandHandler("dni",dni_command))
     app.add_handler(CommandHandler("dnit",dnit_command))
