1e439502ff970f207f52dff0d4653b08dec92a7a
jhanmesa-wq <jhanmesa@inkascollege.edu.pe>
2026-08-22 20:30:55 -0500

    Update bot username and name in main.py

--- main.py
+++ main.py
@@ -282,11 +282,10 @@ async def botones_callback(update:Update, context:ContextTypes.DEFAULT_TYPE):
     
     if q.data=="menu":
         await q.edit_message_text(texto_menu_cmds(), parse_mode="HTML", reply_markup=teclado_menu_cmds())
-
     elif q.data=="cat_reniec":
         await q.edit_message_text("""
 ╔════════════╗
- 🪪 MÓDULO RENIEC
+ 🪪  RENIEC
 ╚════════════╝
 
 ⚡ SISTEMA NACIONAL DE IDENTIDAD ⚡
