aa5f7c1c2bb161e9092af164de5603bac008af27
jhanmesa-wq <jhanmesa@inkascollege.edu.pe>
2026-08-23 22:43:37 -0500

    Update bot username and name in main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from aiohttp import web
 from functools import wraps
 from dotenv import load_dotenv
 from flask import Flask
-from telegram import Update, MessageEntity, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+from telegram import Update, InputMediaPhoto, MessageEntity, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import, Application, CommandHandler, ContextTypes, CallbackQueryHandler
 
 load_dotenv()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
