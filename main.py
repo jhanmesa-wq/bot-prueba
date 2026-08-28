e1a5d3d42078145f432595e1ed8d0f9f7504facc
jhanmesa-wq <jhanmesa@inkascollege.edu.pe>
2026-08-28 12:35:39 -0500

    Update bot username and name in main.py

--- main.py
+++ main.py
@@ -3906,13 +3906,8 @@ def main():
     app.add_handler(CommandHandler("verificar", verificar))
     app.add_handler(CommandHandler("cancelar", cancelar_cmd))
     app.add_handler(CommandHandler("spam", spam))
-app.add_handler(CallbackQueryHandler(callback_spam, pattern=r"^spam_"))
-app.add_handler(
-    MessageHandler(
-        filters.TEXT & ~filters.COMMAND,
-        procesar_spam
-    )
-)
+    app.add_handler(CallbackQueryHandler(callback_spam, pattern=r"^spam_"))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,procesar_spam))
     
     logger.info("⚜️ SPECTER FUTURISTA ONLINE")
     app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
